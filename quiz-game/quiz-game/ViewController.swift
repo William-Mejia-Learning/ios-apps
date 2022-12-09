725d04edd2c8f29b64db48e145fb70a27cd5d913
William Mejia <willx1995@gmail.com>
2022-12-09 13:05:17 -0600

    first commit setting up repository

---
+++ quiz-game/quiz-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz-game
+//
+//  Created by William Mejia on 12/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
