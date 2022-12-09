53e63cd4d343523ab9cff496ac6740bdd11ae694
William Mejia <willx1995@gmail.com>
2022-12-09 14:38:38 -0600

    display text

--- quiz-game/quiz-game/ViewController.swift
+++ quiz-game/quiz-game/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        private let imageView :
+        view.backgroundColor = .systemBlue
+        
+        let labelRect = CGRect(x:50, y:100, width: 200, height: 100)
+        
+        let colorView = UIView(frame: labelRect)
+        colorView.backgroundColor = .red
+        
+        let label = UILabel(frame: labelRect)
+
+        label.textColor = .white
+
+        label.text = "My name is William"
+
+        view.addSubview(label)
+        
+        
     }
 
 
