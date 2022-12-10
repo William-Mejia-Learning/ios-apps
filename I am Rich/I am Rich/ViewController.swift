aa74b820cfb936495af32c3f9a6a833a0f43ad27
William Mejia <willx1995@gmail.com>
2022-12-09 22:10:05 -0600

    I am rich app done, made with the story board

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by William Mejia on 12/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
